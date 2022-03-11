6a501d83ad3316a8fd8f543fcd9335d25d0d3df4
Davi Ferreira <daviferreirart@gmail.com>
2022-03-11 19:25:15 -0300

    book services

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -5,7 +5,15 @@ import BooksServices from "../services/bookServices";
 const app = express()
 
 app.get('/books/author', async (req, res) => {
+    const { nome } = req.body
+    const livros = await BooksServices.getAllBooksWithAuthorName(nome)
+    return res.status(200).json(livros)
+})
 
+app.get('/books/isbn', async (req, res) => {
+    const { isbn } = req.body
+    const isbnLivro = await BooksServices.getBookByISBN(isbn)
+    return res.status(200).json(isbnLivro)
 })
 
 export default app
