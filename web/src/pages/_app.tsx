d0e1bdbef55167cafd77e1bd638c8d9e54cf7470
Davi Ferreira <daviferreirart@gmail.com>
2022-03-12 15:24:01 -0300

    head title

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Your Books</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
