091d9765aa47a9c34c2711d9f742588757d6da1d
Davi Ferreira <daviferreirart@gmail.com>
2022-03-12 20:03:16 -0300

    secret

--- web/src/pages/api/auth/[...nextauth].ts
+++ web/src/pages/api/auth/[...nextauth].ts
@@ -7,5 +7,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
-  ]
+  ],
+  secret : process.env.NEXTAUTH_SECRET || "123"
 })
