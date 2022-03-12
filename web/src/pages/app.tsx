fb3267ee4adf4efc36aa786ac666d701817d68ea
Davi Ferreira <daviferreirart@gmail.com>
2022-03-12 20:46:00 -0300

    removing unused packages

--- web/src/pages/app.tsx
+++ web/src/pages/app.tsx
@@ -8,8 +8,5 @@ export default function App() {
         className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         onClick={() => signOut({ callbackUrl: '/' })}>Sair</button>
     </div>
-
-
-
   )
 }
