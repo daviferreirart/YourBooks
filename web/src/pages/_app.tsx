76e6d79a2b71c5f17811a4b6271272cd3ae15e3c
Davi Ferreira <daviferreirart@gmail.com>
2022-03-12 15:09:37 -0300

    fix web error

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps}/>
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />
 }
 
 export default MyApp
