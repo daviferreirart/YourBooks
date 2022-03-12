13a1c5f0ac0230ec84d6ba3df15e0280a2a28387
Davi Ferreira <daviferreirart@gmail.com>
2022-03-12 20:41:01 -0300

    single user

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,10 +6,11 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req })
 
   if (session) {
+
     return {
       redirect: {
         destination: '/app',
-        permanent: false
+        permanent: false,
       }
     }
   }
