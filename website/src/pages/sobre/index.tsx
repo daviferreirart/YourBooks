7b85f3eb377ae96b2358a4bf47a7d5a9158ff6ad
Davi Ferreira <daviferreirart@gmail.com>
2022-05-14 10:52:32 -0300

    update sobre

--- website/src/pages/sobre/index.tsx
+++ website/src/pages/sobre/index.tsx
@@ -1,52 +1,52 @@
-import React from 'react'
-import { Container, Content, Infos, Name, Name2, Name3, Intro } from '../../styles/sobre/styles'
-import { FaGoogle, FaReact } from 'react-icons/fa'
-import { SiTypescript, SiHeroku, SiPostgresql, SiNextdotjs} from 'react-icons/si'
-import ItemContact from '../../styles/sobre/index'
+import React from "react";
+import {
+  Container,
+  Content,
+  Infos,
+  Name,
+  Name2,
+  Name3,
+  Intro,
+} from "../../styles/sobre/styles";
+import { FaGoogle, FaReact } from "react-icons/fa";
+import {
+  SiTypescript,
+  SiHeroku,
+  SiPostgresql,
+  SiNextdotjs,
+  SiVercel,
+  SiRailway
+} from "react-icons/si";
+import ItemContact from "../../styles/sobre/index";
 
 export default function sobre() {
   return (
     <Container>
       <Content>
+        <Infos>
+          <Name>Sobre o site</Name>
+          <Intro>
+            Site criado como projeto para a disciplina de Estágio Supervisionado
+            II do curso de Sistemas de Informação da UNI7.
+          </Intro>
+        </Infos>
 
-      <Infos>
-        <Name>Sobre o site</Name>
-        <Intro>Site criado como projeto para a disciplina de Estágio Supervisionado II do curso de Sistemas de Informação da UNI7.
-        </Intro>
-      </Infos>
+        <Name2>Equipe:</Name2>
+        <Infos>
+          <Intro>Charlyane Santiago e Davi Ferreira</Intro>
+        </Infos>
 
-      <Name2>Equipe:</Name2>
-      <Infos>
-        <Intro>Charlyane Santiago e Davi Ferreira</Intro>
-      </Infos>
+        <Name2>Tecnologias:</Name2>
 
-      <Name2>Tecnologias:</Name2>
-
-        <ItemContact 
-          IconFa={SiTypescript} 
-          LinkContact="Typescript" 
-        />
-        <ItemContact 
-          IconFa={FaReact} 
-          LinkContact="React" 
-        />
-        <ItemContact 
-          IconFa={SiNextdotjs} 
-          LinkContact="Next.js" 
-        />
-        <ItemContact 
-          IconFa={FaGoogle} 
-          LinkContact="API Google Books" 
-        />
-        <ItemContact 
-          IconFa={SiHeroku} 
-          LinkContact="Heroku" 
-        />
-         <ItemContact 
-          IconFa={SiPostgresql} 
-          LinkContact="PostGree SQL" 
-        />
+        <ItemContact IconFa={SiTypescript} LinkContact="Typescript" />
+        <ItemContact IconFa={FaReact} LinkContact="React" />
+        <ItemContact IconFa={SiNextdotjs} LinkContact="Next.js" />
+        <ItemContact IconFa={FaGoogle} LinkContact="API Google Books" />
+        <ItemContact IconFa={SiHeroku} LinkContact="Heroku" />
+        <ItemContact IconFa={SiPostgresql} LinkContact="PostGree SQL" />
+        <ItemContact IconFa={SiVercel} LinkContact="Vercel" />
+        <ItemContact IconFa={SiRailway} LinkContact="Railway" />
       </Content>
     </Container>
-  )
+  );
 }
